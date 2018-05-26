da8be35fe7b87a4a8bd124f023a38b08a490f2a9
Julien <julien.alexandre26@gmail.com>
2018-05-26 18:46:32 +0200

    Features OK

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -6,33 +6,44 @@ const Features = props => {
     return (
         <div className='features'>
             <h1 className='text-center mb-5'><strong>About</strong> Appone</h1>
-            <p className='text-center mb-5'>Lorem ipsum madolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor coli incididunt ut labore Lorem ipsum madolor sit amet, consectetur adipisicing incididunt.</p>
+            <p className='text-center mb-5'>Description.</p>
 
             <Row>
                 <Col md={6} className='mb-5'>
-                    <Col md={12}>
-                        <div className='text-center mb-4'>
-                            <FA name='dropbox' className='icon' />
-                        </div>
-                        <h5 className='text-center'>Exclusive Design</h5>
-                        <p className='text-center'>Lorem ipsum dolor sit amet, consectet adop adipisicing elit, sed do eiusmod temporara incididunt ugt labore.</p>
-                    </Col>
-                    <Col md={12}>
-                        <div className='text-center mb-4'>
-                            <FA name='cogs' className='icon' />
-                        </div>
-                        <h5 className='text-center'>Easy to Customize</h5>
-                        <p className='text-center'>Lorem ipsum dolor sit amet, consectet adop adipisicing elit, sed do eiusmod temporara incididunt ugt labore.</p>
-                    </Col>
-                    <Col md={12}>
-                        <div className='text-center mb-4'>
-                            <FA name='scissors' className='icon' />
-                        </div>
-                        <h5 className='text-center'>Awesome Features</h5>
-                        <p className='text-center'>Lorem ipsum dolor sit amet, consectet adop adipisicing elit, sed do eiusmod temporara incididunt ugt labore.</p>
-                    </Col>
-
-                    
+                    <Row className='mb-5'>
+                        <Col md={3}  className='order-sm-2'>
+                            <div className='text-center mb-4'>
+                                <FA name='dropbox' className='icon' />
+                            </div>
+                        </Col>
+                        <Col md={9}>
+                            <h5 className='text-center text-lg-right'>Exclusive Design</h5>
+                            <p className='text-center text-lg-right'>Lorem ipsum dolor sit amet, consectet adop adipisicing elit, sed do eiusmod temporara incididunt ugt labore.</p>
+                        </Col>
+                    </Row>
+                   
+                    <Row className='mb-5'>
+                        <Col md={3}  className='order-sm-2'>
+                            <div className='text-center mb-4'>
+                                <FA name='dropbox' className='icon' />
+                            </div>
+                        </Col>
+                        <Col md={9}>
+                            <h5 className='text-center text-lg-right'>Exclusive Design</h5>
+                            <p className='text-center text-lg-right'>Lorem ipsum dolor sit amet, consectet adop adipisicing elit, sed do eiusmod temporara incididunt ugt labore.</p>
+                        </Col>
+                    </Row>
+                    <Row className='mb-5'>
+                        <Col md={3}  className='order-sm-2'>
+                            <div className='text-center mb-4'>
+                                <FA name='dropbox' className='icon' />
+                            </div>
+                        </Col>
+                        <Col md={9}>
+                            <h5 className='text-center text-lg-right'>Exclusive Design</h5>
+                            <p className='text-center text-lg-right'>Lorem ipsum dolor sit amet, consectet adop adipisicing elit, sed do eiusmod temporara incididunt ugt labore.</p>
+                        </Col>
+                    </Row>
                 </Col>
                 <Col md={6} className='mb-5'>
                     Jolies photos
