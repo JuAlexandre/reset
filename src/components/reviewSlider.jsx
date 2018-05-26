e05522ee61c07ad6f06241a12988d96b4b8d6576
Paul <paul@Pauls-MacBook-Pro.local>
2018-05-26 18:51:37 +0200

    save

--- src/components/reviewSlider.jsx
+++ src/components/reviewSlider.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Slider from "react-slick";
+import React from 'react';
+import Slider from 'react-slick';
 
 import Review from './../components/review';
 
@@ -13,29 +13,34 @@ export default class SimpleSlider extends React.Component {
       slidesToScroll: 1,
       arrows: false,
       autoplay: true,
-      autoplaySpeed: 3000
+      autoplaySpeed: 3000,
     };
     return (
-      <Slider {...settings}>
-      <Review 
-          img="http://via.placeholder.com/100x100" 
-          name="John DOE" 
-          role="role0" 
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ullam voluptatibus aliquam aperiam dolor in quia enim non quidem repellendus velit voluptatum consectetur inventore quasi porro mollitia, sit vero asperiores!"
-        />
-        <Review 
-          img="http://via.placeholder.com/100x100" 
-          name="Zen HITUDE" 
-          role="role1" 
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ullam voluptatibus aliquam aperiam dolor in quia enim non quidem repellendus velit voluptatum consectetur inventore quasi porro mollitia, sit vero asperiores!"
-        />
-        <Review 
-          img="http://via.placeholder.com/100x100" 
-          name="Roger RABBIT" 
-          role="role2" 
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ullam voluptatibus aliquam aperiam dolor in quia enim non quidem repellendus velit voluptatum consectetur inventore quasi porro mollitia, sit vero asperiores!"
-        />
-      </Slider>
+      <div>
+        <h2>
+          <strong>CUSTOMER</strong> REVIEWS
+        </h2>
+        <Slider {...settings}>
+          <Review
+            img="http://via.placeholder.com/100x100"
+            name="John DOE"
+            role="role0"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ullam voluptatibus aliquam aperiam dolor in quia enim non quidem repellendus velit voluptatum consectetur inventore quasi porro mollitia, sit vero asperiores!"
+          />
+          <Review
+            img="http://via.placeholder.com/100x100"
+            name="Zen HITUDE"
+            role="role1"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ullam voluptatibus aliquam aperiam dolor in quia enim non quidem repellendus velit voluptatum consectetur inventore quasi porro mollitia, sit vero asperiores!"
+          />
+          <Review
+            img="http://via.placeholder.com/100x100"
+            name="Roger RABBIT"
+            role="role2"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ullam voluptatibus aliquam aperiam dolor in quia enim non quidem repellendus velit voluptatum consectetur inventore quasi porro mollitia, sit vero asperiores!"
+          />
+        </Slider>
+      </div>
     );
   }
 }
