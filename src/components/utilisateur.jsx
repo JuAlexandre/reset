ac1bd9885dd959e5fcffd67c6d4c68b266bc3397
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-05-27 13:30:39 +0200

    ajout du logo profil dans l'entête

---
+++ src/components/utilisateur.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Col, Button } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
+
+const Utilisateur = props => {
+  return (
+    <div>
+        <FontAwesome name="user" size="2x" />
+    </div>
+  );
+};
+
+export default Utilisateur;
--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -3,20 +3,31 @@ import { Container, Row, Col, Button, Label, Input, FormGroup } from 'reactstrap
 
 import NavbarApp from './../components/navbarApp';
 import Calendar from './../components/calendar/calendar';
+import Utilisateur from './../components/utilisateur';
 
 const CalendarPage = () => (
   <div>
-    <div className="title">
-      <h4>Réservation</h4>
-      <p>Réservez votre Cocon</p>
-    </div>
+    <Container fluid className="title">
+      <Row>
+        <Col col={10}>
+          <h4>Réservation</h4>
+          <p>Réservez votre Cocon</p>
+        </Col>
+        <Col col={2} className="d-flex align-items-center justify-content-end pr-5">
+          <Row>
+            <Utilisateur />
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+    
     <Container fluid className="marginBottom">
       <Row className="d-flex justify-content-center mt-5">
         <Calendar />
       </Row>
       
       <Row className="mt-5 d-flex justify-content-center">
-        <h3>Crénaux disponibles</h3>
+        <h3>Créneaux disponibles</h3>
       </Row>
       <Row className="d-flex justify-content-around mt-4">
         <FormGroup check className="mx-3" >
