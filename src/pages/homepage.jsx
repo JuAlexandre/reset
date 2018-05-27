e5097fe515e91d9a49481765a52bd4328ccdeb11
Paul <paul@Pauls-MacBook-Pro.local>
2018-05-27 16:55:58 +0200

    prefix

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
-import { Container, Row, Col, Button , Label, Input, Form } from 'reactstrap';
-import Link from 'gatsby-link';
+import { Container, Row, Col, Button, Label, Input, Form } from 'reactstrap';
+import Link, { withPrefix } from 'gatsby-link';
 import NavbarApp from './../components/navbarApp';
 
 const HomepagePage = () => (
-    <div className='homepage'>
-        <div className="title">
-            <h4>RESET</h4>
-        </div>
-        <Container>
-            <Row className='d-flex justify-content-center mt-5 mb-3'>
-                <img src="./static/imgs/default-user-profile.svg" alt="profile img"/>
-            </Row>
-            <Row className='d-flex justify-content-center mb-3'>
-                <h1>Bienvenue <span className='primaryColor'>John</span>.</h1>
-            </Row>
-            <Row className='d-flex justify-content-center'>
-                <p>
-                    Prochaine réservation le <span className='primaryColor'>28/05/2018</span> à <span className='primaryColor'>12h45</span>.
-                </p>
-                <p className='text-center mx-3'>Vous avez sélectionné l'ambiance <span className='primaryColor'>nature</span>, vous pouvez modifier votre choix à tout moment dans vos paramètres.</p>
-            </Row>
-        </Container>
-        <NavbarApp className="marginBottom" />
+  <div className="homepage">
+    <div className="title">
+      <h4>RESET</h4>
     </div>
+    <Container>
+      <Row className="d-flex justify-content-center mt-5 mb-3">
+        <img
+          src={withPrefix('./static/imgs/default-user-profile.svg')}
+          alt="profile img"
+        />
+      </Row>
+      <Row className="d-flex justify-content-center mb-3">
+        <h1>
+          Bienvenue <span className="primaryColor">John</span>.
+        </h1>
+      </Row>
+      <Row className="d-flex justify-content-center">
+        <p>
+          Prochaine réservation le{' '}
+          <span className="primaryColor">28/05/2018</span> à{' '}
+          <span className="primaryColor">12h45</span>.
+        </p>
+        <p className="text-center mx-3">
+          Vous avez sélectionné l'ambiance{' '}
+          <span className="primaryColor">nature</span>, vous pouvez modifier
+          votre choix à tout moment dans vos paramètres.
+        </p>
+      </Row>
+    </Container>
+    <NavbarApp className="marginBottom" />
+  </div>
 );
 
 export default HomepagePage;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,43 +1,57 @@
 import React from 'react';
-import { Container, Row, Col, Button , Label, Input, Form } from 'reactstrap';
-import Link from 'gatsby-link';
+import { Container, Row, Col, Button, Label, Input, Form } from 'reactstrap';
+import Link, { withPrefix } from 'gatsby-link';
 
 const LoginPage = () => (
-    <div className='loginBody'>
-        <Container className="marginBottom mb-5">
-            <Row className='d-flex justify-content-center pt-5 mb-5'>
-                <img src="./static/imgs/logo_white.png" alt="reset" className='loginLogo'/>
-            </Row>
-            <hr/>
-            <h2 className='text-center mt-5'>Connecte toi !</h2>
-            <Form className='ml-3 mr-3'>
-                <Row className='mb-3'>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" value="john.doe@gmail.com" />
-                </Row>
-                <Row className='mb-5'>
-                    <Label for="examplePassword">Mot de passe</Label>
-                    <Input type="password" name="password" id="examplePassword" value="password" />
-                </Row>
-                <div className='d-flex justify-content-center'>
-                    <Row>
-                        <Col>
-                            <Link to="/homepage">
-                                <Button className='button'>Se connecter</Button>
-                            </Link>
-                        </Col>
-                    </Row>
-                </div>
-                <div className='d-flex justify-content-center mt-3'>
-                    <Row>
-                        <Col>
-                            <p>S'inscrire</p>
-                        </Col>
-                    </Row>
-                </div>
-            </Form>
-        </Container>
-    </div>
+  <div className="loginBody">
+    <Container className="marginBottom mb-5">
+      <Row className="d-flex justify-content-center pt-5 mb-5">
+        <img
+          src={withPrefix('./static/imgs/logo_white.png')}
+          alt="reset"
+          className="loginLogo"
+        />
+      </Row>
+      <hr />
+      <h2 className="text-center mt-5">Connecte toi !</h2>
+      <Form className="ml-3 mr-3">
+        <Row className="mb-3">
+          <Label for="exampleEmail">Email</Label>
+          <Input
+            type="email"
+            name="email"
+            id="exampleEmail"
+            value="john.doe@gmail.com"
+          />
+        </Row>
+        <Row className="mb-5">
+          <Label for="examplePassword">Mot de passe</Label>
+          <Input
+            type="password"
+            name="password"
+            id="examplePassword"
+            value="password"
+          />
+        </Row>
+        <div className="d-flex justify-content-center">
+          <Row>
+            <Col>
+              <Link to="/homepage">
+                <Button className="button">Se connecter</Button>
+              </Link>
+            </Col>
+          </Row>
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+          <Row>
+            <Col>
+              <p>S'inscrire</p>
+            </Col>
+          </Row>
+        </div>
+      </Form>
+    </Container>
+  </div>
 );
 
 export default LoginPage;
