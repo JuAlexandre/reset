6061ff355f3ae3677edd1ef6425c7a1c8dbcaa25
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-05-27 13:38:12 +0200

    liens blancs

--- src/components/utilisateur.jsx
+++ src/components/utilisateur.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Container, Col, Button } from 'reactstrap';
 import FontAwesome from 'react-fontawesome';
+import Link from 'gatsby-link';
+
 
 const Utilisateur = props => {
   return (
     <div>
-        <FontAwesome name="user" size="2x" />
+        <Link to="/homepage">
+          <FontAwesome name="user" size="2x" />
+        </Link>
     </div>
   );
 };
