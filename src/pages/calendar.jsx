380577d58cb73c9b81c7497ef4708ef2d411ccf7
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-05-27 12:14:04 +0200

    ajout crénaux disponibles

--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col, Button, Label, Input, FormGroup } from 'reactstrap';
 
 import NavbarApp from './../components/navbarApp';
 import Calendar from './../components/calendar/calendar';
@@ -13,6 +13,44 @@ const CalendarPage = () => (
     <Container fluid className="marginBottom">
       <Row className="d-flex justify-content-center mt-5">
         <Calendar />
+      </Row>
+      
+      <Row className="mt-5 d-flex justify-content-center">
+        <h3>Crénaux disponibles</h3>
+      </Row>
+      <Row className="d-flex justify-content-around mt-4">
+        <FormGroup check className="mx-3" >
+          <Label check>
+            <Input type="checkbox" />{' '}
+              11:55
+          </Label>
+        </FormGroup>
+        <FormGroup check className="mx-3" >
+          <Label check>
+            <Input type="checkbox" />{' '}
+              12:05
+          </Label>
+        </FormGroup>
+        <FormGroup check className="mx-3" >
+          <Label check>
+            <Input type="checkbox" />{' '}
+              12:10
+          </Label>
+        </FormGroup>
+      </Row>
+      <Row className="d-flex justify-content-around mt-4">
+        <FormGroup check className="mx-3" >
+          <Label check>
+            <Input type="checkbox" />{' '}
+              12:55
+          </Label>
+        </FormGroup>
+        <FormGroup check className="mx-3" >
+          <Label check>
+            <Input type="checkbox" />{' '}
+              13:15
+          </Label>
+        </FormGroup>
       </Row>
       <Row className="d-flex justify-content-center mt-5">
           <Button className="button-inverse">Réserver</Button>
